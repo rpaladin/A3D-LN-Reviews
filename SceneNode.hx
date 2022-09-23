137ce026776aed2a746a2f8d5c6ce3908831dd4b
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-09-23 15:00:38 -0700

    Review

--- SceneNode.hx
+++ SceneNode.hx
@@ -1,31 +1,18 @@
 package armory.logicnode;
 
-import iron.Scene;
-import iron.data.SceneFormat.TSceneFormat;
-import iron.data.Data;
-
 class SceneNode extends LogicNode {
 
 	public var property0: String;
-	public var value: Scene;
 
 	public function new(tree: LogicTree) {
 		super(tree);
-
-		iron.Scene.active.notifyOnInit(function() {
-			get(0);
-		});
 	}
 
 	override function get(from: Int): Dynamic {
-		if (property0 != null) {
-			value = iron.Scene.active.root.children.get(property0);
-		}
-
-		return value;
+		return property0;
 	}
 
 	override function set(value: Dynamic) {
-		this.value = value;
+		this.property0 = value;
 	}
 }
